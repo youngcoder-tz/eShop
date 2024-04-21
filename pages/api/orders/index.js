8be3e84ab1ff282fc111403b69c1680d5b6b366f
Numo Francis <numofran6@gmail.com>
2024-04-21 19:40:14 +0000

    Log no session

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
 	if (!session) {
 		console.log('nosesh');
-		return req?.status(401).send('sigin required');
+		return res.status(401).send('sigin required');
 	}
 
 	const user = session;
